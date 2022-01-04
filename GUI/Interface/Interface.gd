a4b0da6d4523375ed04c06f2b0f393f8b34dea66
jkaspereit <j.kaspereit1994@gmail.com>
2022-01-04 14:29:08 +0100

    google play login

--- GUI/Interface/Interface.gd
+++ GUI/Interface/Interface.gd
@@ -3,6 +3,11 @@ extends CanvasLayer
 onready var ScoreValue = $DeadScreen/VBoxContainer/HBoxContainer/ScoreValue
 onready var CoinValue = $DeadScreen/VBoxContainer/HBoxContainer2/CoinValue
 
+onready var PlayIntegration = get_node("/root/PlayIntegration")
+
+func _ready():
+	PlayIntegration.sign_in();
+
 func show_StartingArea():
 	$StartArea.visible = true
 
