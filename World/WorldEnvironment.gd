b7d782b07c356ae7d607a10e57693771c8e389ff
jkaspereit <j.kaspereit1994@gmail.com>
2022-01-19 11:24:30 +0100

    blur

--- World/WorldEnvironment.gd
+++ World/WorldEnvironment.gd
@@ -1,8 +1,30 @@
 extends WorldEnvironment
 
+func to_shop():
+	$AnimationPlayer.play("Blur");
+
+func to_leaderboard():
+	$AnimationPlayer.play("Blur");
+
+func to_selection():
+	$AnimationPlayer.play("Blur");
+
+func to_settings():
+	$AnimationPlayer.play("Blur");
+
+func to_home(from):
+	$AnimationPlayer.play_backwards("Blur")
 
 func game_over():
-	environment.dof_blur_far_enabled = true
+	blur_on()
 
 func at_home():
-	environment.dof_blur_far_enabled = false
+	blur_off()
+
+func blur_on():
+	environment.dof_blur_far_amount = 0.13
+	environment.dof_blur_near_amount = 0.13
+
+func blur_off():
+	environment.dof_blur_far_amount = 0
+	environment.dof_blur_near_amount = 0
